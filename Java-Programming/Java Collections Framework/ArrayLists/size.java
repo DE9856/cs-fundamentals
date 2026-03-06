c0ff8794e9650a69af22513167aa440f840da215
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-03-06 22:40:00 +0530

    Create size.java

---
+++ Java-Programming/Java Collections Framework/ArrayLists/size.java
@@ -0,0 +1,11 @@
+//Find the size of an ArrayList of fruits.
+import java.util.*;
+public class test17{
+    public static void main(String args[]){
+        ArrayList <Integer> list = new ArrayList <Integer>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println(list.size());
+    }
+}
