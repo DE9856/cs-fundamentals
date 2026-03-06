0ea9617201ad339738ffb07dd819625e88e3ff64
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-03-06 22:38:58 +0530

    Create is_empty.java

---
+++ Java-Programming/Java Collections Framework/ArrayLists/is_empty.java
@@ -0,0 +1,10 @@
+//Check if an ArrayList is empty or not and print “Empty” or “Not Empty”.
+
+import java.util.*;
+public class test15{
+    public static void main(String args[]){
+        ArrayList <Integer> list = new ArrayList <Integer>();
+        if(list.isEmpty())
+            System.out.println("List is Empty");
+    }
+}
