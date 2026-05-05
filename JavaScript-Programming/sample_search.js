ed338c57a813700121b33d78c3a27172324896a0
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-05-05 19:15:58 +0530

    Update sample_search.js

--- JavaScript-Programming/sample_search.js
+++ JavaScript-Programming/sample_search.js
@@ -1,35 +1,24 @@
 import { useState } from 'react';
 
-function App() {
-  const initiallist = ['Mango', 'Banana', 'Samosa', 'Maggi', 'Bonda'];
-  const [list, updatelist] = useState(initiallist);
-
-  const filterlist = (event) => {
+function App(){
+  const initiallist = ['Mango','Maggi','Banana','Apple','Watermelon'];
+  const [list,updatelist] = useState(initiallist);
+  const filterlist = (event) =>{
     const value = event.target.value.toLowerCase();
-
-    if (value === "") {
-      updatelist(initiallist);
-      return;
-    }
-
     const filtered = initiallist.filter(item =>
-      item.toLowerCase().includes(value)
-    );
-
+      item.toLowerCase().includes(value));
     updatelist(filtered);
   };
-
   return (
-    <div className="App">
+    <div className = "App">
       <label>Search: </label>
-      <input type="text" onChange={filterlist} />
+      <input type  = "text" onChange = {filterlist} />
       <ul>
-        {list.map((element) => (
-          <li key={element}>{element}</li>
+        {list.map((element)=>(
+          <li key = {element}>{element}</li>
         ))}
       </ul>
     </div>
-  );
+  )
 }
-
 export default App;
