1bcfe156b96ebe8669fb62b6bf21d0cd4f25eeae
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-05-25 16:02:23 +0530

    Update prims_algorithm.c

--- C-Programming/ADA/Graph/prims_algorithm.c
+++ C-Programming/ADA/Graph/prims_algorithm.c
@@ -2,47 +2,50 @@
 #include <stdlib.h>
 #define INF 999
 
-void prim(int adj[10][10], int n){
+void prim(int n,int a[n][n]){
     int min;
-    int u = 0, v=0;
-    int cost = 0;
-    int visited[10] = {0};
-    visited[1]=1;
+    int u = 0,v=0;
+    int cost=0;
+    int visited[n];
+    for(int i =0;i<n;i++)
+        visited[i] = 0; 
+    visited[0] = 1;
 
     while(1){
         min = INF;
-        for(int i=1;i<=n;i++){
+        for(int i =0;i<n;i++){
             if(visited[i]){
-                for(int j=1;j<=n;j++){
-                    if(!visited[j] && adj[i][j]<min){
-                        min = adj[i][j];
-                        u=i;
-                        v=j;
+                for(int j = 0;j<n;j++){
+                    if(!visited[j] && a[i][j]<min){
+                        min = a[i][j];
+                        u = i;
+                        v = j;
                     }
                 }
             }
         }
         if(min==INF)
             break;
-        visited[v] = 1;
-        printf("\nEdge: ( %d -> %d ): %d", u, v, min);
+        printf("\nEdge ( %d -> %d ): %d", u+1,v+1,min);
         cost+=min;
+        visited[v] = 1;
     }
-    printf("\nThe minimum cost is: %d",cost);
+    printf("The total path cost: %d", cost);
 }
 
 void main(){
-    int adj[10][10];
     int n;
-    printf("Enter the number of nodes: ");
+    printf("Enter the number of vertices: ");
     scanf("%d", &n);
-    printf("Enter the adjacency matrix: ");
-    for(int i=1;i<=n;i++)
-        for(int j = 1;j<=n;j++){
-            scanf("%d", &adj[i][j]);
-            if(i!=j && adj[i][j]==0)
-                adj[i][j] = INF;
+    int a[n][n];
+    printf("Enter the cost adjacency matrix: ");
+    for(int i = 0;i<n;i++){
+        for(int j =0;j<n;j++){
+            scanf("%d", &a[i][j]);
+            if(i!=j && a[i][j]==0)
+                a[i][j] = INF;
         }
+    }
     printf("Prim's Algorithm: ");
-    prim(adj,n);
+    prim(n,a);
 }
