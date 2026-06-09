b7626ade1ef77d8b2a711d7e934cbb3aad8dd9b1
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-06-09 17:18:30 +0530

    Update 6.sql

--- SQL/6.sql
+++ SQL/6.sql
@@ -6,42 +6,39 @@ The cursor will merge the data available in the newly create table N_rollcall
 create database exp6;
 use exp6;
 
-create table o_rollcall(roll_no int, name varchar(20), address varchar(20));
-create table n_rollcall(roll_no int, name varchar(20), address varchar(20));
+create table o_rollcall(id int, name varchar(20), address varchar(20));
+create table n_rollcall(id int, name varchar(20), address varchar(20));
 
-insert into o_rollcall values(1, 'Deepesh', 'Bengaluru');
-insert into o_rollcall values(2, 'Deepesh1', 'Hyderabad');
+insert into o_rollcall values(1,'Deepesh', 'Bengaluru');
+insert into o_rollcall values(2,'Deepesh1', 'Hyderabad');
 
 DELIMITER $$
-CREATE PROCEDURE p3(IN r1 INT)
-BEGIN
-    DECLARE r2 INT;
-    DECLARE c1 CURSOR FOR
-        SELECT roll_no
-        FROM o_rollcall
-        WHERE roll_no > r1;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND
-        SET @FINISH = 1;
-    OPEN c1;
-    SET @FINISH = 0;
-    cursor_loop: LOOP
-        FETCH c1 INTO r2;
-        IF @FINISH = 1 THEN
-            LEAVE cursor_loop;
-        END IF;
-        IF NOT EXISTS
-        (
-            SELECT *
-            FROM n_rollcall
-            WHERE roll_no = r2
-        )
-        THEN
-            INSERT INTO n_rollcall
-            SELECT *
-            FROM o_rollcall
-            WHERE roll_no = r2;
-        END IF;
-    END LOOP;
-    CLOSE c1;
-END $$ 
+create procedure p3 (in r1 int)
+begin
+	declare r2 int;
+    declare c1 cursor for 
+		select id from o_rollcall where id > r1;
+	declare continue handler for not found
+		set @FINISH=1;
+	
+    open c1;
+		set @FINISH=0;
+        cursor_loop: LOOP
+			fetch c1 into r2;
+            if @FINISH=1 then
+				leave cursor_loop;
+			end if;
+			if not exists( select * from n_rollcall where id = r2) then
+				insert into n_rollcall
+					select * from o_rollcall where id = r2;
+			end if;
+		end loop;
+	close c1;
+	end $$
 DELIMITER ;
+
+
+call p3(0);
+select * from n_rollcall;
+
+
