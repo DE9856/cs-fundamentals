0feb84041e3b2f950ecb572c9a95e15a3ad8f38e
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-06-09 17:07:53 +0530

    Refactor stored procedure p3 for clarity and efficiency

--- SQL/6.sql
+++ SQL/6.sql
@@ -12,27 +12,36 @@ create table n_rollcall(roll_no int, name varchar(20), address varchar(20));
 insert into o_rollcall values(1, 'Deepesh', 'Bengaluru');
 insert into o_rollcall values(2, 'Deepesh1', 'Hyderabad');
 
-delimiter $$
-create procedure p3(in r1 int)
-begin
-  declare r2 int;
-declare exit_loop boolean;
-declare c1 cursor for select roll_no from o_rollcall where roll_no > r1;
-declare continue handler for not found
-  set exit_loop = true;
-
-open c1:
-  e_loop: loop
-    fetch c1 into r2;
-    if not exists (select * from n_rollcall where roll_no = r2) then
-      insert into n_rollcall
-        select * from o_rollcall where roll_no = r2;
-    end if;
-
-  if exit_loop then
-    close c1;
-  leave e_loop;
-end if;
-end loop;
-end $$
+DELIMITER $$
+CREATE PROCEDURE p3(IN r1 INT)
+BEGIN
+    DECLARE r2 INT;
+    DECLARE c1 CURSOR FOR
+        SELECT roll_no
+        FROM o_rollcall
+        WHERE roll_no > r1;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND
+        SET @FINISH = 1;
+    OPEN c1;
+    SET @FINISH = 0;
+    cursor_loop: LOOP
+        FETCH c1 INTO r2;
+        IF @FINISH = 1 THEN
+            LEAVE cursor_loop;
+        END IF;
+        IF NOT EXISTS
+        (
+            SELECT *
+            FROM n_rollcall
+            WHERE roll_no = r2
+        )
+        THEN
+            INSERT INTO n_rollcall
+            SELECT *
+            FROM o_rollcall
+            WHERE roll_no = r2;
+        END IF;
+    END LOOP;
+    CLOSE c1;
+END $$ 
 DELIMITER ;
