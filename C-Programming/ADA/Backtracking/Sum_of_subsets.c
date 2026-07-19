16a151192df27a31a2b9f1faf6b64828a7045acc
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-07-19 20:08:06 +0530

    Update Sum_of_subsets.c

--- C-Programming/ADA/Backtracking/Sum_of_subsets.c
+++ C-Programming/ADA/Backtracking/Sum_of_subsets.c
@@ -1,40 +1,43 @@
 #include <stdio.h>
-#define MAX 50
-int s[MAX], x[MAX], d,n;
+#include <stdlib.h>
+#define MAX 100
 
-void sumofsubsets(int p, int k, int r){
+int s[MAX],x[MAX],n,d;
+
+void sumofsubset(int p, int k, int r){
     if(k>n)
         return;
-    x[k] = 1;
+    x[k]=1;
     if(p+s[k]==d){
-        for(int i =1;i<=k;i++){
+        for(int i=1;i<=n;i++){
             if(x[i])
-                printf(" %d ", s[i]);
+                printf(" %d ", s[i]);    
         }
         printf("\n");
+        x[k]=0;
     }
-    else if(k+1<=n && (p+s[k]+s[k+1]<=d)){
-        sumofsubsets(p+s[k], k+1, r-s[k]);
+    else if(k+1<=n && p+s[k]+s[k+1]<=d){
+        sumofsubset(p+s[k], k+1, r-s[k]);
     }
+    x[k] = 0;
     if(k+1<=n && (p+r-s[k]>=d) && (p+s[k+1]<=d)){
-        x[k] = 0;
-        sumofsubsets(p,k+1,r-s[k]);
+        sumofsubset(p,k+1,r-s[k]);
     }
 }
 
 void main(){
-    int sum = 0;
-    printf("Enter length of the array: ");
+    printf("Enter the number of elements: ");
     scanf("%d", &n);
-    printf("Enter the elements in ascending order: ");
-    for(int i =1;i<=n;i++){
+    int sum=0;
+    printf("Enter the elements: ");
+    for(int i=1;i<=n;i++){
         scanf("%d", &s[i]);
         sum+=s[i];
     }
-    printf("Enter the sum value: ");
+    printf("Enter the target sum: ");
     scanf("%d", &d);
     if(sum<d || s[1]>d)
-        printf("No subsets possible.");
+        printf("No subsets available");
     else
-        sumofsubsets(0,1,sum);
+        sumofsubset(0,1,sum);
 }
