0ffd9a64b4e3222e895d9bb3ca270dd6c1a7d187
Deepesh Kumar Kotta <82948768+DE9856@users.noreply.github.com>
2026-07-19 14:31:37 +0530

    Update 01_knapsack.c

--- C-Programming/ADA/Knapsack/01_knapsack.c
+++ C-Programming/ADA/Knapsack/01_knapsack.c
@@ -4,42 +4,61 @@
 int profit[20], weight[20], dp[20][20];
 int n,m;
 
+void printDP(){
+    printf("\nDP Table\n");
+    for(int i=0;i<=n;i++){
+        for(int j = 0;j<=m;j++)
+            printf("%d ",dp[i][j]);
+        printf("\n");
+    }
+}
+
 void kp(){
     for(int i=0;i<=n;i++){
-        for(int w = 0;w<=m;w++){
+        for(int w=0;w<=m;w++){
             if(i==0 || w==0)
-                dp[i][w] = 0;
+                dp[i][w]=0;
             else if(weight[i-1]<=w)
                 dp[i][w] = max(dp[i-1][w], profit[i-1]+dp[i-1][w-weight[i-1]]);
             else
                 dp[i][w] = dp[i-1][w];
         }
     }
-    printf("The max profit is: %d\nSolution: ",dp[n][m]);
+
+    printDP();
+
+    printf("The maximum profit is: %d\n Solution: ",dp[n][m]);
     int r = dp[n][m];
     int w = m;
     int x[20] = {0};
-    for(int i = n; i>0 && r>0 ;i--){
+    for(int i=n;i>0 && r>0;i--){
         if(r!=dp[i-1][w]){
             x[i-1] = 1;
             r-=profit[i-1];
             w-=weight[i-1];
         }
     }
-    for(int i = 0;i<n;i++)
-        printf(" %d ", x[i]);
+
+    for(int i=0;i<n;i++)
+        printf("%d ",x[i]);
+    printf("\n");
 }
 
-void main(){
+int main(){
     printf("Enter the number of objects: ");
     scanf("%d", &n);
+
     printf("Enter the profits: ");
-    for(int i =0;i<n;i++)
+    for(int i=0;i<n;i++)
         scanf("%d", &profit[i]);
+    
     printf("Enter the weights: ");
-    for(int i = 0;i<n;i++)
+    for(int i=0;i<n;i++)
         scanf("%d", &weight[i]);
+
     printf("Enter the capacity: ");
     scanf("%d", &m);
+
     kp();
+    return 0;
 }
